c2ba8b2eea4a8296df4959c62d720467cd6fc1aa
Chris Johnson <chrisltd@gmail.com>
2017-10-27 12:23:18 -0400

    added smooth scroll and active toggle

--- src/js/script.js
+++ src/js/script.js
@@ -11,4 +11,21 @@ import { consoleSafe } from "./helpers.js";
 
   console.log('Hello World!');
 
+  // Smooth scrolling
+  $('a[href*=#]:not([href=#])').click(function() {
+    console.log('smooth scroll!');
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') || location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      smoothScrollTo( target );
+    }
+  });
+
+  // Toggle active
+  $('[data-toggle-active]').click(function(event) {
+    event.preventDefault();
+    var $toggleTarget = $( $(this).data('toggle-active') );
+    $toggleTarget.toggleClass('active');
+  });
+
 })(jQuery);
